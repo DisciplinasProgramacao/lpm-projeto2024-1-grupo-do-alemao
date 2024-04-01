caae4be8a152c1c389f3c2a20b280542bf87ffbd
thais-reiss <thaisthatareis@gmail.com>
2024-04-01 10:27:48 -0300

    atualização fila

--- codigo/src/FilaDeEspera.java
+++ codigo/src/FilaDeEspera.java
@@ -7,10 +7,12 @@ public class FilaDeEspera {
         numRequisicoes = 0;
     }
 
+    //Método para adicionar uma requisição na fila
     public void addRequisicaoNaFila(Requisicao requisicao) {
             requisicoes[numRequisicoes++] = requisicao;
     }
 
+    //Método para remover uma requição na fila pelo número de pessoas
     public void removerRequisicaoDafiLa(int numPessoas) {
         for (int posicao = 0; posicao < numRequisicoes; posicao++) {
             if(numPessoas == requisicoes[posicao].getPessoas()){
@@ -26,11 +28,29 @@ public class FilaDeEspera {
         }
     }
 
+    //Método para remover requisição da fila pelo cliente
+    public void removerRequisicaoDafiLa(Cliente cliente) {
+        for (int posicao = 0; posicao < numRequisicoes; posicao++) {
+            if(cliente == requisicoes[posicao].getCliente()){
+                
+                for(int i = posicao + 1; i < numRequisicoes; i++ ) {
+                    requisicoes[i - 1] = requisicoes[i];
+                }
+
+                requisicoes[numRequisicoes - 1] = null;
+                numRequisicoes--;
+                return;
+            }
+        }
+    }
+
+    //Método para recuperar o número de requisições presentes na lista
     public int getNumRequisicoes() {
         return numRequisicoes;
     }
 
-    public String imprimirRequisicoes() {
+    //Método para recuperar as requisições no total
+    public String getRequisicoes() {
         String resultado = "";
         for(int i = 0; i < numRequisicoes; i ++) {
             resultado+=(requisicoes[i].getPessoas());
--- codigo/src/TestFilaDeEspera.java
+++ codigo/src/TestFilaDeEspera.java
@@ -1,12 +1,11 @@
-public class TestFilaDeEspera {
-    import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
- 
+
+public class TestFilaDeEspera { 
     @Test
     public void testAdicionaRequisicao() {
         FilaDeEspera fila = new FilaDeEspera();
-        Requisicao req1 = new Requisicao(3);
+        Requisicao req1 = new Requisicao(3, null);
         fila.addRequisicaoNaFila(req1);
         assertEquals(1, fila.getNumRequisicoes());
     }
@@ -14,8 +13,8 @@ import org.junit.Test;
     @Test
     public void testRemoveRequisicao(){
         FilaDeEspera fila = new FilaDeEspera();
-        Requisicao req1 = new Requisicao(4);
-        Requisicao req2 = new Requisicao(3);
+        Requisicao req1 = new Requisicao(4, null);
+        Requisicao req2 = new Requisicao(3, null);
         fila.addRequisicaoNaFila(req1);
         fila.addRequisicaoNaFila(req2);
         fila.removerRequisicaoDafiLa(3);
@@ -25,12 +24,25 @@ import org.junit.Test;
     @Test
     public void testRemoveApenasAPrimeiraReq() {
         FilaDeEspera fila = new FilaDeEspera();
-        Requisicao req1 = new Requisicao(3);
-        Requisicao req2 = new Requisicao(3);
+        Requisicao req1 = new Requisicao(3, null);
+        Requisicao req2 = new Requisicao(3, null);
         fila.addRequisicaoNaFila(req1);
         fila.addRequisicaoNaFila(req2);
         assertEquals(2, fila.getNumRequisicoes());
         fila.removerRequisicaoDafiLa(3);
+        assertEquals(1, fila.getNumRequisicoes());
+    }
+
+    @Test
+    public void testRemoveRequisicaoCliente(){
+        FilaDeEspera fila = new FilaDeEspera();
+        Cliente c = new Cliente();
+        Cliente c1 = new Cliente();
+        Requisicao req1 = new Requisicao(0, c);
+        Requisicao req2 = new Requisicao(0, c1);
+        fila.addRequisicaoNaFila(req1);
+        fila.addRequisicaoNaFila(req2);
+        fila.removerRequisicaoDafiLa(c);
         assertEquals(1, fila.getNumRequisicoes());
     }
 }
