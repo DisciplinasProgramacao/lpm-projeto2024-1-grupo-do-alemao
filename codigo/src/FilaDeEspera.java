a67f781dff06f2891050496418a201fea6fcfcea
thais-reiss <thaisthatareis@gmail.com>
2024-04-07 12:58:38 -0300

    Atualização fila de espera

--- codigo/src/FilaDeEspera.java
+++ codigo/src/FilaDeEspera.java
@@ -1,23 +1,23 @@
 public class FilaDeEspera {
-    private Requisicao[] requisicoes; 
+    private RequisicaoReserva[] requisicoes;
     private int numRequisicoes;
 
-    public FilaDeEspera () {
-        requisicoes = new Requisicao[100];
+    public FilaDeEspera() {
+        requisicoes = new RequisicaoReserva[100];
         numRequisicoes = 0;
     }
 
-    //Método para adicionar uma requisição na fila
-    public void addRequisicaoNaFila(Requisicao requisicao) {
-            requisicoes[numRequisicoes++] = requisicao;
+    // Método para adicionar uma requisição na fila
+    public void addRequisicaoNaFila(RequisicaoReserva requisicao) {
+        requisicoes[numRequisicoes++] = requisicao;
     }
 
-    //Método para remover uma requição na fila pelo número de pessoas
+    // Método para remover uma requição na fila pelo número de pessoas
     public void removerRequisicaoDafiLa(int numPessoas) {
         for (int posicao = 0; posicao < numRequisicoes; posicao++) {
-            if(numPessoas == requisicoes[posicao].getPessoas()){
-                
-                for(int i = posicao + 1; i < numRequisicoes; i++ ) {
+            if (numPessoas == requisicoes[posicao].getPessoas()) {
+
+                for (int i = posicao + 1; i < numRequisicoes; i++) {
                     requisicoes[i - 1] = requisicoes[i];
                 }
 
@@ -28,12 +28,12 @@ public class FilaDeEspera {
         }
     }
 
-    //Método para remover requisição da fila pelo cliente
+    // Método para remover requisição da fila por um objeto do tipo Cliente
     public void removerRequisicaoDafiLa(Cliente cliente) {
         for (int posicao = 0; posicao < numRequisicoes; posicao++) {
-            if(cliente == requisicoes[posicao].getCliente()){
-                
-                for(int i = posicao + 1; i < numRequisicoes; i++ ) {
+            if (cliente == requisicoes[posicao].getCliente()) {
+
+                for (int i = posicao + 1; i < numRequisicoes; i++) {
                     requisicoes[i - 1] = requisicoes[i];
                 }
 
@@ -44,16 +44,25 @@ public class FilaDeEspera {
         }
     }
 
-    //Método para recuperar o número de requisições presentes na lista
+    // Método para recuperar o número de requisições presentes na fila
     public int getNumRequisicoes() {
         return numRequisicoes;
     }
 
-    //Método para recuperar as requisições no total
+    // Método para recuperar o número de pessoas de todas as requisições da fila
     public String getRequisicoes() {
         String resultado = "";
-        for(int i = 0; i < numRequisicoes; i ++) {
-            resultado+=(requisicoes[i].getPessoas());
+        for (int i = 0; i < numRequisicoes; i++) {
+            resultado += requisicoes[i].getPessoas() + " ";
+        }
+        return resultado;
+    }
+
+    // Método para recuperar os nomes dos clientes que estão na fila
+    public String getRequisicoesCliente() {
+        String resultado = "";
+        for (int i = 0; i < numRequisicoes; i++) {
+            resultado += requisicoes[i].getCliente().getNome() + " ";
         }
         return resultado;
     }
