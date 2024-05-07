dbb09b42a85f9b8f03e05a24ff7be2ebee449328
Maira46 <145265675+Maira46@users.noreply.github.com>
2024-05-07 14:33:41 -0300

    Update Pedido.java

--- codigo/src/Pedido.java
+++ codigo/src/Pedido.java
@@ -1,25 +1,54 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A classe Pedido representa um pedido de produtos.
+ */
 public class Pedido {
     private List<Produto> pedido;
 
+    /**
+     * Construtor da classe Pedido.
+     * Inicializa a lista de produtos do pedido.
+     */
     public Pedido() {
         this.pedido = new ArrayList<Produto>();
     }
 
+    /**
+     * Adiciona um produto ao pedido.
+     * 
+     * @param produto O produto a ser adicionado ao pedido.
+     */
     public void addProduto(Produto produto) {
         this.pedido.add(produto);
     }
 
+    /**
+     * Remove um produto do pedido.
+     * 
+     * @param produto O produto a ser removido do pedido.
+     */
     public void removerProduto(Produto produto) {
         this.pedido.remove(produto);
     }
 
+    /**
+     * Obtém o pedido completo.
+     * 
+     * @return A lista de produtos no pedido.
+     */
     public List<Produto> getPedido() {
         return this.pedido;
     }
 
+    /**
+     * Fecha o pedido, incluindo uma taxa de serviço de 10%.
+     * 
+     * @param pessoas O número de pessoas para dividir o valor total.
+     * @return Um array contendo o valor total do pedido e o valor a ser dividido
+     *         igualmente entre os ocupantes.
+     */
     public double[] fecharPedido(int pessoas) {
         double totalSemTaxa = 0.0;
         for (Produto produto : this.pedido) {
