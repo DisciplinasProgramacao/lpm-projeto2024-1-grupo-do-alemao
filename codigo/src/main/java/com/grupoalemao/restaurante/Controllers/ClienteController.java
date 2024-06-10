83a08fc261b1f49fe08bb52627b41f2d4d0ce2f1
ReblablA <111395018+ReblablA@users.noreply.github.com>
2024-06-10 09:26:51 -0300

    Update ClienteController.java

---
+++ codigo/src/main/java/com/grupoalemao/restaurante/Controllers/ClienteController.java
@@ -0,0 +1,27 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    private List<Cliente> clientes = new ArrayList<>();
+
+    @PostMapping
+    public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente) {
+        clientes.add(cliente);
+        return new ResponseEntity<>(cliente, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> obterCliente(@PathVariable int id) {
+        if (id < 0 || id >= clientes.size()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(clientes.get(id), HttpStatus.OK);
+    }
+}
