32208f3e5583f4a943ca6a5ae9d74659ae5480be
ReblablA <111395018+ReblablA@users.noreply.github.com>
2024-06-10 09:47:12 -0300

    Update RequisicaoReserva.java

--- codigo/RequisicaoReserva.java
+++ codigo/RequisicaoReserva.java
@@ -3,14 +3,13 @@ import java.time.LocalDate;
 /**
  * Classe RequisicaoReserva representa uma solicitação de reserva em um
  * restaurante.
- * Cada instância desta classe armazena informações sobre a reserva, como data,
+ * Cada instância desta classe armazena informações sobre a reserva,como o
  * número de pessoas, cliente associado e mesa reservada.
  * Esta classe controla também o status da reserva.
  */
 public class RequisicaoReserva {
     private static int proximoId = 1;
     private int id;
-    private LocalDate dataReserva;
     private boolean ativa;
     private int pessoas;
     private Cliente cliente;
@@ -22,15 +21,12 @@ public class RequisicaoReserva {
      * Inicializa uma reserva com os parâmetros fornecidos e gera um identificador
      * único automaticamente.
      * Marca a mesa associada como indisponível.
-     *
-     * @param dataReserva A data da reserva.
      * @param pessoas     O número de pessoas na reserva.
      * @param cliente     O cliente associado à reserva.
      * @param mesa        A mesa reservada.
      */
     public RequisicaoReserva(LocalDate dataReserva, int pessoas, Cliente cliente, Mesa mesa) {
         this.id = proximoId++;
-        this.dataReserva = dataReserva;
         this.ativa = true;
         this.pessoas = pessoas;
         this.cliente = cliente;
@@ -48,14 +44,6 @@ public class RequisicaoReserva {
         return id;
     }
 
-    /**
-     * Método para obter a data da reserva.
-     *
-     * @return A data da reserva.
-     */
-    public LocalDate getDataReserva() {
-        return dataReserva;
-    }
 
     /**
      * Método para verificar se a reserva está ativa.
