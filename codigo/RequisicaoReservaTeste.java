d70aa55806ee317f547d7241532a0a9295963af6
ReblablA <111395018+ReblablA@users.noreply.github.com>
2024-06-22 12:27:20 -0300

    Update RequisicaoReservaTeste.java

--- codigo/RequisicaoReservaTeste.java
+++ codigo/RequisicaoReservaTeste.java
@@ -6,47 +6,53 @@ import org.junit.jupiter.api.Test;
 
 public class RequisicaoReservaTeste {
 
-@Test
-public void testeIsAtivaVerdadeiro() {
-Cliente cliente1 = new Cliente("João","98878932");
-Mesa mesa1 = new Mesa(1,6,true,cliente1);
+    @Test
+    public void testeIsAtivaVerdadeiro() {
+        Cliente cliente1 = new Cliente("João", "98878932");
+        Mesa mesa1 = new Mesa(1, 6, true, cliente1);
 
-LocalDate dataReserva = LocalDate.now();
-int pessoas = 4;
-RequisicaoReserva requisicao1 = new RequisicaoReserva(dataReserva, pessoas, cliente1, mesa1);
+        LocalDate dataReserva = LocalDate.now();
+        int pessoas = 4;
+        RequisicaoReserva requisicao1 = new RequisicaoReserva(dataReserva, pessoas, cliente1, mesa1);
 
-assertTrue("A reserva deve estar ativa antes do cancelamento",requisicao1.isAtiva());
-}
+        assertTrue("A reserva deve estar ativa antes do cancelamento", requisicao1.isAtiva());
+    }
 
-@Test
-public void testeIsAtivaFalso() {
-Cliente cliente1 = new Cliente("João","98878932");
-Mesa mesa1 = new Mesa(1,6,true,cliente1);
+    @Test
+    public void testeIsAtivaFalso() {
+        Cliente cliente1 = new Cliente("João", "98878932");
+        Mesa mesa1 = new Mesa(1, 6, true, cliente1);
 
-LocalDate dataReserva = LocalDate.now();
-int pessoas = 4;
-RequisicaoReserva requisicao1 = new RequisicaoReserva(dataReserva, pessoas, cliente1, mesa1);
+        LocalDate dataReserva = LocalDate.now();
+        int pessoas = 4;
+        RequisicaoReserva requisicao1 = new RequisicaoReserva(dataReserva, pessoas, cliente1, mesa1);
 
-requisicao1.cancelar();
-assertFalse("A reserva deve estar cancelada após o cancelamento", requisicao1.isAtiva());
-}
-/*@Test
-public void testarFuncionalidadesRestaurante() {
-Cliente cliente1 = new Cliente("João","98878932");
-Mesa mesa1 = new Mesa(1,6,true,cliente1);
+        requisicao1.cancelar();
+        assertFalse("A reserva deve estar cancelada após o cancelamento", requisicao1.isAtiva());
+    }
 
-LocalDate dataReserva = LocalDate.now();
-int pessoas = 4;
-RequisicaoReserva requisicao1 = new RequisicaoReserva(dataReserva, pessoas, cliente1, mesa1);
+    @Test
+    public void testarFuncionalidadesRestaurante() {
+        Cliente cliente1 = new Cliente("João", "98878932");
+        Mesa mesa1 = new Mesa(1, 6, true, cliente1);
 
-assertTrue("A reserva deve estar ativa antes do cancelamento", requisicao1.isAtiva());
-requisicao1.cancelar();
-assertFalse("A reserva deve estar cancelada após o cancelamento", requisicao1.isAtiva());
+        LocalDate dataReserva = LocalDate.now();
+        int pessoas = 4;
+        RequisicaoReserva requisicao1 = new RequisicaoReserva(dataReserva, pessoas, cliente1, mesa1);
 
+        // Testa se a reserva está ativa inicialmente
+        assertTrue("A reserva deve estar ativa antes do cancelamento", requisicao1.isAtiva());
 
-mesa1.setDisponivel(false);
-assertFalse(true, mesa1.isDisponivel(),"A mesa deve estar ocupada antes da liberação");
-//mesa1.liberar();
-// assertTrue("A mesa deve estar livre após a liberação", mesa1.isDisponivel());
-}*/
+        // Cancela a reserva e verifica se está inativa
+        requisicao1.cancelar();
+        assertFalse("A reserva deve estar cancelada após o cancelamento", requisicao1.isAtiva());
+
+        // Verifica se a mesa está ocupada
+        mesa1.setDisponivel(false);
+        assertFalse("A mesa deve estar ocupada antes da liberação", mesa1.isDisponivel());
+
+        // Libera a mesa e verifica se está disponível
+        mesa1.liberar();
+        assertTrue("A mesa deve estar livre após a liberação", mesa1.isDisponivel());
+    }
 }
