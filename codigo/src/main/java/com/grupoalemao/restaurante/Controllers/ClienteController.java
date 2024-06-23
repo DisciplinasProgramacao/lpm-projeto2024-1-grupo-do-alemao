a52f55b81e69850fa3f34e0acc874c35cf5be189
ReblablA <111395018+ReblablA@users.noreply.github.com>
2024-06-23 16:48:33 -0300

    Update ClienteController.java

--- codigo/src/main/java/com/grupoalemao/restaurante/Controllers/ClienteController.java
+++ codigo/src/main/java/com/grupoalemao/restaurante/Controllers/ClienteController.java
@@ -1,31 +1,37 @@
 package com.grupoalemao.restaurante.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import com.grupoalemao.restaurante.Models.Cliente;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
-    private List<Cliente> clientes = new ArrayList<>();//* arrumar para BD
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @PostMapping
     public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente) {
-        clientes.add(cliente);
-        return new ResponseEntity<>(cliente, HttpStatus.CREATED);
+        Cliente savedCliente = clienteRepository.save(cliente);
+        return new ResponseEntity<>(savedCliente, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Cliente> obterCliente(@PathVariable int id) {
-        if (id < 0 || id >= clientes.size()) {
+    public ResponseEntity<Cliente> obterCliente(@PathVariable Integer id) {
+        return clienteRepository.findById(id)
+                .map(cliente -> new ResponseEntity<>(cliente, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCliente(@PathVariable Integer id) {
+        if (clienteRepository.existsById(id)) {
+            clienteRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(clientes.get(id), HttpStatus.OK);
     }
 }
